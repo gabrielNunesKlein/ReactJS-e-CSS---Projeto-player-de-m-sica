940ea345a86fb1738ec38a9540e60713a0f2f8cd
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-22 19:29:34 -0300

    PlayList e finalização do projeto

--- src/containers/Player.tsx
+++ src/containers/Player.tsx
@@ -1,10 +1,14 @@
-import { headerInfo } from "../services/api";
+import { headerInfo, tracks } from "../services/api";
 import PlaylistHeader from "./components/PlaylistHeader"
+import PlaylistItem from "./components/PlaylistItem";
 
 const Player = () => {
     return (
         <div className="player-container">
             <PlaylistHeader {...headerInfo} />
+            {tracks.map((track) => (
+                <PlaylistItem { ...track} key={track.id} />
+            ))}
         </div>
     );
 };
